ce4bbd702bb8a35ca4fc28064f8997fd9e93fe96
Rahul <rahulxdagar@gmail.com>
2022-04-16 22:51:21 +0530

    Second Commit

--- app/src/main/java/com/example/mysignlang/MainActivity.java
+++ app/src/main/java/com/example/mysignlang/MainActivity.java
@@ -25,5 +25,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        button=findViewById(R.id.phrasebutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),PhraseList.class);
+                startActivity(intent);
+            }
+        });
     }
 }
