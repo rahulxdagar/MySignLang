a43d07c3618c6d818780385785af632e82156a9e
Rahul <rahulxdagar@gmail.com>
2022-04-16 22:30:24 +0530

    First Commit

---
+++ app/src/main/java/com/example/mysignlang/CharList.java
@@ -0,0 +1,14 @@
+package com.example.mysignlang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CharList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_char_list);
+    }
+}
---
+++ app/src/main/java/com/example/mysignlang/PhraseList.java
@@ -0,0 +1,14 @@
+package com.example.mysignlang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhraseList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phrase_list);
+    }
+}
