14125d2a549a7d63ed16aef189e124eff8e11730
Rahul <rahulxdagar@gmail.com>
2022-04-19 00:10:17 +0530

    Last Commit with almost all features working

--- app/src/main/java/com/example/mysignlang/PhraseList.java
+++ app/src/main/java/com/example/mysignlang/PhraseList.java
@@ -3,6 +3,7 @@ package com.example.mysignlang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class PhraseList extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class PhraseList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phrase_list);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.wrapper1, new recfragment2()).commit();
     }
 }
--- app/src/main/java/com/example/mysignlang/myadapter2.java
+++ app/src/main/java/com/example/mysignlang/myadapter2.java
@@ -14,9 +14,16 @@ import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 
-public class myadapter extends FirebaseRecyclerAdapter<models,myadapter.myviewholder> {
+public class myadapter2 extends FirebaseRecyclerAdapter<models,myadapter2.myviewholder> {
 
-    public myadapter(@NonNull FirebaseRecyclerOptions<models> options) {
+
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public myadapter2(@NonNull FirebaseRecyclerOptions<models> options) {
         super(options);
     }
 
@@ -30,25 +37,24 @@ public class myadapter extends FirebaseRecyclerAdapter<models,myadapter.myviewho
             @Override
             public void onClick(View view) {
                 AppCompatActivity activity=(AppCompatActivity) view.getContext();
-                activity.getSupportFragmentManager().beginTransaction().replace(R.id.wrapper,new descfragment(model.getName(),model.getDesc(),model.getPurl())).addToBackStack(null).commit();
+                activity.getSupportFragmentManager().beginTransaction().replace(R.id.wrapper1,new descfragment2(model.getName(),model.getDesc(),model.getPurl())).addToBackStack(null).commit();
 
             }
         });
-
     }
 
     @NonNull
     @Override
     public myviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.singlerowdesign,parent,false);
-        return new myviewholder(view);
+        return new myadapter2.myviewholder(view);
     }
 
     public class myviewholder extends RecyclerView.ViewHolder{
-        ImageView img1;
-        TextView nametext,desctext;
+            ImageView img1;
+            TextView nametext,desctext;
 
-        public myviewholder(@NonNull View itemView) {
+            public myviewholder(@NonNull View itemView) {
             super(itemView);
 
             img1=itemView.findViewById(R.id.img1);
